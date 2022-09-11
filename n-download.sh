08e543fcd79efc0d823aab925d227c9b9cd6550f
alexj <furydeathx@gmail.com>
2022-09-11 07:29:22 +0700

    changed urls

--- n-download.sh
+++ n-download.sh
@@ -9,9 +9,9 @@ rm -rf nadekobot_old 1>/dev/null 2>&1
 mv -fT nadekobot nadekobot_old 1>/dev/null 2>&1
 
 # clone new version
-git clone -b v4 --recursive --depth 1 https://gitlab.com/Kwoth/nadekobot
+git clone -b v4 --recursive --depth 1 https://github.com/epikgallavoir/nedakoc
 
-wget -q -N https://gitlab.com/Kwoth/nadeko-bash-installer/-/raw/v4/rebuild.sh
+wget -q -N https://raw.githubusercontent.com/epikgallavoir/bashdeko/main/rebuild.sh
 bash rebuild.sh
 
 cd "$root"
