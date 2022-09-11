8656ba131716fa3e2c2330b5ed8c4c0f6106340a
alexj <furydeathx@gmail.com>
2022-09-11 07:33:43 +0700

    changed urls

--- linuxAIO.sh
+++ linuxAIO.sh
@@ -5,7 +5,7 @@ echo "Downloading the latest installer..."
 root=$(pwd)
 
 rm "$root/n-menu.sh" 1>/dev/null 2>&1
-wget -N https://gitlab.com/Kwoth/nadeko-bash-installer/-/raw/v4/n-menu.sh
+wget -N https://raw.githubusercontent.com/epikgallavoir/bashdeko/main/n-menu.sh
 
 bash n-menu.sh
 cd "$root"
