09fb84e97e0b9b93633b0e43cef72c9d166a2c36
alexj <61342323+epikgallavoir@users.noreply.github.com>
2022-09-18 20:26:41 +0700

    fixed

--- n-arn.sh
+++ n-arn.sh
@@ -27,12 +27,12 @@ read choice
     if [ $choice -eq 1 ] ; then
         echo ""
         echo "Running nedakoc with auto update. Please wait. . ."
-        while :; do cd $root/nadekoc/output && dotnet NadekoBot.dll ; youtube-dl -U && cd $root && wget -N https://raw.githubusercontent.com/epikgallavoir/bashdeko/main/n-download.sh && bash "$root/n-download.sh"; sleep 5s; done
+        while :; do cd $root/nedakoc/output && dotnet NadekoBot.dll ; youtube-dl -U && cd $root && wget -N https://raw.githubusercontent.com/epikgallavoir/bashdeko/main/n-download.sh && bash "$root/n-download.sh"; sleep 5s; done
         echo "Done"
     elif [ $choice -eq 2 ] ; then
         echo ""
         echo "Running nedakoc without auto update. Please wait. . ."
-        while :; do cd "$root/nadekoc/output" && dotnet NadekoBot.dll && youtube-dl -U; sleep 5s; done
+        while :; do cd "$root/nedakoc/output" && dotnet NadekoBot.dll && youtube-dl -U; sleep 5s; done
         echo "Done"
     elif [ "$choice" -eq 3 ] ; then
         echo ""
